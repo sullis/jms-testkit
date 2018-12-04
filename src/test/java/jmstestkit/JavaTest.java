3ca008fbd80e2f50744035278f55c31fbe39a218
Sean Sullivan <github@seansullivan.com>
2018-12-04 12:00:08 -0800

    tweak JavaTest.java

--- src/test/java/jmstestkit/JavaTest.java
+++ src/test/java/jmstestkit/JavaTest.java
@@ -3,16 +3,17 @@ package jmstestkit;
 
 import com.google.common.collect.Lists;
 import org.testng.annotations.*;
-
-import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.*;
 
 public class JavaTest {
 
     @Test
-    public void sanityCheck() throws Exception {
+    public void sanityCheck()  {
         JmsQueue queue = JmsQueue.apply();
         queue.publishMessage("Hello");
         queue.publishMessage("Bonjour");
+        assertNotNull(queue.queueName());
+        assertNotNull(queue.createConnectionFactory());
         assertEquals(queue.size(), 2);
         assertEquals(queue.toJavaList(), Lists.newArrayList("Hello", "Bonjour"));
     }
