bbd4d994744f1b4676b2aa00a6bfe82482e23a8e
sullis <github@seansullivan.com>
2018-12-31 12:18:38 -0800

    add trait: JmsTestKit (#2)

---
+++ src/main/scala/jmstestkit/JmsTestKit.scala
@@ -0,0 +1,30 @@
+package jmstestkit
+
+trait JmsTestKit {
+  def withBroker()(test: JmsBroker => Unit): Unit = {
+    val broker = JmsBroker()
+    try {
+      test(broker)
+      Thread.sleep(500)
+    } finally {
+      broker.stop()
+    }
+  }
+
+  def withConnectionFactory()(test: javax.jms.ConnectionFactory => Unit): Unit = {
+    withBroker() { broker =>
+      test(broker.createConnectionFactory)
+    }
+  }
+
+  def withQueue()(test: JmsQueue => Unit): Unit = {
+    val queue = JmsQueue()
+    try {
+      test(queue)
+      Thread.sleep(500)
+    } finally {
+      queue.broker.stop()
+    }
+  }
+
+}
