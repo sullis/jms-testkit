49012f2d8165300d870325b18aca38ea9ec16c0a
Sean C. Sullivan <github@seansullivan.com>
2020-12-30 19:37:37 -0800

    fix JmsTestKitSpec

--- src/test/scala/jmstestkit/JmsTestKitSpec.scala
+++ src/test/scala/jmstestkit/JmsTestKitSpec.scala
@@ -8,56 +8,58 @@ class JmsTestKitSpec
   with Matchers
   with JmsTestKit {
 
-  def connectionFactorySanityCheck: Unit = {
-    withConnectionFactory() { cf =>
-      val conn = cf.createConnection
-      conn.start()
-      conn.stop()
-      conn.close()
+  "JmsTestKit " should {
+    "withConnectionFactory happy path" in {
+      withConnectionFactory() { cf =>
+        val conn = cf.createConnection
+        conn.start()
+        conn.stop()
+        conn.close()
+      }
     }
-  }
 
-  def brokerSanityCheck: Unit = {
-    withBroker() { broker =>
-      broker.isStarted shouldBe true
-      broker.isStopped shouldBe false
-      val cf = broker.createConnectionFactory
-      val conn = cf.createConnection
-      conn.start()
-      conn.stop()
-      conn.close()
+    "withBroker happy path" in {
+      withBroker() { broker =>
+        broker.isStarted shouldBe true
+        broker.isStopped shouldBe false
+        val cf = broker.createConnectionFactory
+        val conn = cf.createConnection
+        conn.start()
+        conn.stop()
+        conn.close()
+      }
     }
-  }
 
-  def queueSanityCheck: Unit = {
-    withQueue() { queue =>
-      queue.size shouldBe 0
-      queue.publishMessage("Hello world")
-      queue.size shouldBe 1
-      val broker = queue.broker
-      broker.isStarted shouldBe true
-      broker.isStopped shouldBe false
-      val cf = broker.createConnectionFactory
-      val conn = cf.createConnection
-      conn.start()
-      conn.stop()
-      conn.close()
+    "withQueue" in {
+      withQueue() { queue =>
+        queue.size shouldBe 0
+        queue.publishMessage("Hello world")
+        queue.size shouldBe 1
+        val broker = queue.broker
+        broker.isStarted shouldBe true
+        broker.isStopped shouldBe false
+        val cf = broker.createConnectionFactory
+        val conn = cf.createConnection
+        conn.start()
+        conn.stop()
+        conn.close()
+      }
     }
-  }
 
-  def topicSanityCheck: Unit = {
-    withTopic() { topic =>
-      topic.toSeq.size shouldBe 0
-      topic.publishMessage("Hello world")
-      topic.toSeq.size shouldBe 1
-      val broker = topic.broker
-      broker.isStarted shouldBe true
-      broker.isStopped shouldBe false
-      val cf = broker.createConnectionFactory
-      val conn = cf.createConnection
-      conn.start()
-      conn.stop()
-      conn.close()
+    "withTopic" in {
+      withTopic() { topic =>
+        topic.toSeq.size shouldBe 0
+        topic.publishMessage("Hello world")
+        topic.toSeq.size shouldBe 1
+        val broker = topic.broker
+        broker.isStarted shouldBe true
+        broker.isStopped shouldBe false
+        val cf = broker.createConnectionFactory
+        val conn = cf.createConnection
+        conn.start()
+        conn.stop()
+        conn.close()
+      }
     }
   }
 }
